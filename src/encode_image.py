e88bd6c4c5793a86960cf6218c784155f6696a96
jwhang627 <jw8n5@mail.missouri.edu>
2020-05-10 17:31:09 -0500

    update

--- src/encode_image.py
+++ src/encode_image.py
@@ -35,13 +35,32 @@ def get_bitstream(img):
 def main():
     fileName = argv[1]
     image = None
+    height = 0
+    width = 0
     print("> checking if the file " + fileName + " is in /images directory.")
     if path.exists("./images/" + fileName) == False:
         print("> file " + fileName + " doesn't exist in /image.")
+        return None
     else:
         print("> image " + fileName + " found.")
-        image = imread("./images/" + fileName)
-        print("> image size: ",image.shape[0],"x", image.shape[1]".")
+        image = imread("./images/" + fileName,IMREAD_GRAYSCALE)
+        print("> image grayscaled.")
+        height, width = image.shape[0],image.shape[1]
+        print("> image size: " + str(height) + "x" + str(width) + ".")
 
+    # resize if the image cannot be divided by 8x8 block
+    if ((height%bl_size != 0) or (width%bl_size != 0)):
+        height -= height%bl_size
+        width -= width%bl_size
+        image = resize(image,dsize=(width,height))
+    print("> image resized to " + str(height) + "x" + str(width) + ".")
+    num_blocks_h = math.ceil(height/bl_size)
+    num_blocks_w = math.ceil(width/bl_size)
+    #print(num_blocks_h,num_blocks_w)
+    padded_img = np.zeros([height,width])
+    padded_img[0:height,0:width] = image[0:height,0:width]
+    print("> writing uncompressed bitmap.")
+    imwrite("./results/uncompressed.bmp",np.uint8(padded_img))
+    
 if __name__ == '__main__':
     main()
